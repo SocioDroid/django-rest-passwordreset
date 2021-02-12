4498da932c3fff436b3791077918821473568d75
Aishwarya Kendle <47445489+SocioDroid@users.noreply.github.com>
2021-02-12 18:01:10 +0530

    Update views.py

--- django_rest_passwordreset/views.py
+++ django_rest_passwordreset/views.py
@@ -111,7 +111,7 @@ class ResetPasswordRequestToken(GenericAPIView):
         clear_expired(now_minus_expiry_time)
 
         # find a user by email address (case insensitive search)
-        users = User.objects.filter(email=email)
+        users = User.objects.filter(**{'email': email})
 
         active_user_found = False
 
